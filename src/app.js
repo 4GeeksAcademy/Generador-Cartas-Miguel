f059488e9591e5b075f199cc1c164d3a7f26f8c3
magutierrezhijosa <magutierrezhijosa@gmail.com>
2024-12-11 15:29:59 +0000

    Carta aleatoria hecho hasta Boton de borrar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,112 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function numerosToFiguras(numeroCarta) {
+  if (numeroCarta === 11) {
+    numeroCarta = "J";
+  }
+  if (numeroCarta === 12) {
+    numeroCarta = "Q";
+  }
+  if (numeroCarta === 13) {
+    numeroCarta = "K";
+  }
+  if (numeroCarta === 1) {
+    numeroCarta = "A";
+  }
+
+  return numeroCarta;
+}
+
+function crearCarta(numeroPalo, numeroCarta) {
+  let card = document.querySelector(".card");
+  if (numeroPalo === 1) {
+    let cardDiamante =
+      '<div class="paloArribaIzquierda ♦">♦</div>' +
+      '<div class="numeroCarta">' +
+      numeroCarta +
+      "</div>" +
+      '<div class="paloAbajoDerecha ♦">♦</div>';
+
+    card.innerHTML += cardDiamante;
+  }
+  if (numeroPalo === 2) {
+    let cardHearth =
+      '<div class="paloArribaIzquierda ♥ ">♥ </div>' +
+      '<div class="numeroCarta">' +
+      numeroCarta +
+      "</div>" +
+      '<div class="paloAbajoDerecha ♥ ">♥ </div>';
+
+    card.innerHTML += cardHearth;
+  }
+  if (numeroPalo === 3) {
+    let cardPicas =
+      '<div class="paloArribaIzquierda  ♠"> ♠</div>' +
+      '<div class="numeroCarta">' +
+      numeroCarta +
+      "</div>" +
+      '<div class="paloAbajoDerecha  ♠"> ♠</div>';
+
+    card.innerHTML += cardPicas;
+  }
+  if (numeroPalo === 4) {
+    let cardTreboles =
+      '<div class="paloArribaIzquierda ♣">♣</div>' +
+      '<div class="numeroCarta">' +
+      numeroCarta +
+      "</div>" +
+      '<div class="paloAbajoDerecha ♣">♣</div>';
+
+    card.innerHTML += cardTreboles;
+  }
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numeroPalo = Math.floor(Math.random() * 4 + 1);
+  let numeroCarta = Math.floor(Math.random() * 13 + 1);
+
+  let cartasConFiguras = numerosToFiguras(numeroCarta);
+
+  crearCarta(numeroPalo, cartasConFiguras);
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let buttonCambiar = document.querySelector("#cambiarCarta");
+
+buttonCambiar.addEventListener("click", event => {
+  let card = document.querySelector(".card");
+  let numeroPalo = Math.floor(Math.random() * 4 + 1);
+  let numeroCarta = Math.floor(Math.random() * 13 + 1);
+
+  borrarCarta(card);
+
+  let cartasConFiguras = numerosToFiguras(numeroCarta);
+
+  crearCarta(numeroPalo, cartasConFiguras);
+});
+
+function borrarCarta(parent) {
+  let divTop = document.querySelector(".paloArribaIzquierda");
+  let divMid = document.querySelector(".numeroCarta");
+  let divBottom = document.querySelector(".paloAbajoDerecha");
+
+  parent.removeChild(divTop);
+  parent.removeChild(divMid);
+  parent.removeChild(divBottom);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
